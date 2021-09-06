2dcb0581c0c28d95ba1ef5e23180f4482f65eb38
Michael Rabbai <michael.rabbai@gmail.com>
2021-09-06 18:03:07 -0400

    change start/stop button title based on workout state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import Button from './components/Button';
 import Timer from './components/Timer';
 
 function App() {
+  const [hasStarted, setHasStarted] = useState(false);
+
+  const startWorkoutHandler = () => {
+    console.log('start clicked');
+    setHasStarted(prevState => !prevState);
+  };
+
+  const pauseWorkoutHandler = () => {
+    console.log('pause clicked');
+  };
+
   return (
     <main className="global centered">
       <div className="container column">
         <span className="title">Interval Timer</span>
         <Timer duration={5} />
         <div className="button-container">
-          <Button title="Start" onClick={() => console.log('start clicked')} />
-          <Button title="Pause" onClick={() => console.log('stop clicked')} />
+          <Button
+            title={!hasStarted ? 'Start' : 'Stop'}
+            onClick={startWorkoutHandler}
+          />
+          <Button title="Pause" onClick={pauseWorkoutHandler} />
         </div>
       </div>
     </main>
