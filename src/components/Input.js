418713c1ecb3266ed5702d826ab67c3ad60cdc60
Michael Rabbai <michael.rabbai@gmail.com>
2021-09-06 22:03:45 -0400

    add Input cmp styles

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,18 @@
 import { useField } from 'formik';
 
+import classes from './Input.module.css';
+
 const Input = ({ label, ...props }) => {
   const [field, meta] = useField(props);
 
   return (
-    <>
+    <div className={classes.container}>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <input {...field} {...props} />
-      {meta.touched && meta.error && <div>{meta.error}</div>}
-    </>
+      <input {...field} {...props} className={classes.input} />
+      {meta.touched && meta.error && (
+        <div className={classes.error}>{meta.error}</div>
+      )}
+    </div>
   );
 };
 
