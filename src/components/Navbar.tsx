7e247990607b718134db77447dfa6ffde3be8faf
Michael Rabbai <michael.rabbai@gmail.com>
2022-10-13 18:27:33 -0400

    refactor Navbar component to use typescript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import classes from './Navbar.module.css';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <nav className={classes.nav}>
       <span className={classes.title}>Interval Timer</span>
