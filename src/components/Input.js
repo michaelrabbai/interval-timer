64e510872948e7aa8e29718943fcbc143a5ca949
Michael Rabbai <michael.rabbai@gmail.com>
2022-10-13 18:21:11 -0400

    refactor Input component to accept label as a prop

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,12 +2,13 @@ import { useField } from 'formik';
 
 import classes from './Input.module.css';
 
-const Input = ({ label, ...props }) => {
+
+const Input = (props) => {
   const [field, meta] = useField(props);
 
   return (
     <div className={classes.container}>
-      <label htmlFor={props.id || props.name}>{label}</label>
+      <label htmlFor={props.id || props.name}>{props.label}</label>
       <input {...field} {...props} className={classes.input} />
       {meta.touched && meta.error && (
         <div className={classes.error}>{meta.error}</div>
